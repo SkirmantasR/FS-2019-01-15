5f0aa6326d59497597b47335490dd4c6b822f79a
Žilvinas <Zilvinas.vidmantas.93@gmail.com>
2019-02-16 09:18:34 +0200

    done

--- MEDŽIAGA/pamoku darbas/6/main.js
+++ MEDŽIAGA/pamoku darbas/6/main.js
@@ -27,13 +27,30 @@
 // }
 // --------------------
 
-a = prompt("įveskite A");
-b = prompt("įveskite B");
-if (!isNaN(a) && !isNaN(b)) {
-  a = Number(a);
-  b = Number(b)
-  console.log('A = ' + a + ' B = ' + b);
-  console.log('A + B =', + (a + b));
-  console.log('A - B =', (a - b));
-  console.log('A / B liekana:', + (a % b));
-} else alert('Neteisingai įvedėte skaičius');
+// a = prompt("įveskite A");
+// b = prompt("įveskite B");
+// if (!isNaN(a) && !isNaN(b)) {
+//   a = Number(a);
+//   b = Number(b)
+//   console.log('A = ' + a + ' B = ' + b);
+//   console.log('A + B =', + (a + b));
+//   console.log('A - B =', (a - b));
+//   console.log('A / B liekana:', + (a % b));
+// } else alert('Neteisingai įvedėte skaičius');
+// // ----------------------------------------
+
+let username = prompt('Iveskite vartotojo varda');  // vartokite let
+let psw = prompt('Iveskite slaptazodi');            // globaliai aprašyti kintamuosius bloga praktika
+if ((username === "admin") && (psw === "master")) {
+   document.write("Sveiki admin");
+}
+else if ((psw === (username + "11"))) {
+   document.write("Sveiki ", username);
+}
+// Automatiškai duos true, jei netuščias username
+else if ((username) && (psw.length !== 0)) { // psw kitu būdu: viena lygybė reiškia priskirimą,
+   document.write('Neteisingas slaptažodis!'); //  norint tikrinti reikia rašyti '=='
+}
+else {
+   document.write("Viso gero");
+}
