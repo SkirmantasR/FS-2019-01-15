b7718fb470123fd33226c4f022061aaf94f95aac
Žilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2019-02-22 15:49:17 +0200

    Done - stage 1

--- Zilvinas Vidmantas/9/objectTableManager.js
+++ Zilvinas Vidmantas/9/objectTableManager.js
@@ -1,4 +1,3 @@
-// Pavyzdiniai duomenys
 console.table(cars);
 
 // Įeinamieji parametrai
@@ -8,51 +7,85 @@ console.table(cars);
  * @param {*Array} data - Objektų masyvas, su savybe id
  * @param {*HTMLElement} container - Mazgas, turiniui talpinti
  */
-function ObjectTableManager(tableName, data, container) {
-  let propNames = Object.getOwnPropertyNames(data[0]).filter(el => el != "id");
-  let objectBeingEdited = false;
-  // Privatūs kintamieji
-  let tableTitle = '<h1 class="text-dark text-center">' + tableName + "</h1>";
-  let table = document.createElement("table");
+function ObjectManager(tableName, data, container) {
+  let propNames = Object.getOwnPropertyNames(data[0]).filter(el => el != 'id');
+  let tableTitle = document.createElement('h1');
   let tableHeader = formatHeader();
-  let tbody = document.createElement("tbody");
+  let formCreate = formatFormCreate();
+  let table = document.createElement('table');
+  let tbody = document.createElement('tbody');
+  let objectBeingEdited = false;
+  let maxId = data.length;
+
+  addSubmitHandler(formCreate);
+
+  tableTitle.innerHTML = tableName;
+  tableTitle.className = 'text-dark text-center';
   table.className = "table table-striped";
   table.innerHTML += tableHeader;
   table.appendChild(tbody);
-  container.innerHTML += tableTitle;
+
+  container.appendChild(tableTitle);
+  container.appendChild(formCreate);
   container.appendChild(table);
 
-  // Formatuojama lentelės antraštė
+  container.className = "border border-dark rounded p-3 my-3 manager-panel";
+
+  render();
+
+  function formatFormCreate() {
+    let form = document.createElement('form');
+    form.className = 'my-3 p-3';
+    let formContent = `
+    <h3 class="text-sucess">Create Object</h3>
+    <div class="form-row">`;
+    propNames.forEach(el => {
+      formContent += `
+      <div class="form-group col">
+        <label for="${el}">${camelToTitle(el)}</label>
+        <input type="text" class="form-control" id="${el}" name="${el}">
+      </div>`;
+    });
+    formContent += `</div><input type="submit" class="btn btn-success d-block mx-auto" value="Add object">`;
+    form.innerHTML = formContent;
+    return form;
+  }
+
+  function addSubmitHandler(form) {
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      let values = propNames.map(el => form[el].value);
+      let formedObject = formObject(maxId++, values);
+      saveObject(formedObject);
+      render();
+    });
+  }
+  // Formatuojama lentelės antraštės
   function formatHeader() {
     let result = '<thead class="thead-dark"><tr>';
-    propNames.forEach(el => (result += "<th>" + camelToHeader(el) + "</th>"));
+    propNames.forEach(el => (result += "<th>" + camelToTitle(el) + "</th>"));
     return result + "<th></th></thead>";
   }
 
-  // Keičia objekto savybę į headerį: labasAsZmogus -> Labas as Zmogus
-  function camelToHeader(camelCase) {
-    camelCase = camelCase.charAt(0).toUpperCase() + camelCase.slice(1); // Pirmają parašome didžiają
+  function camelToTitle(camelCase) {
+    camelCase = camelCase.charAt(0).toUpperCase() + camelCase.slice(1);
     for (let i = 1; i < camelCase.length; i++) {
-      // Pradedame ieškoti nuo antros raidės (nes pirma turi būt didžioji)
       if (camelCase[i] == camelCase[i].toUpperCase()) {
-        // Jeigu didžioji
         camelCase =
           camelCase.slice(0, i) +
           camelCase.charAt(i).toLowerCase() +
-          camelCase.slice(i + 1); // Paverčiame  mažąja
-        camelCase = camelCase.slice(0, i) + " " + camelCase.slice(i++); // įterpiamas tarpas
+          camelCase.slice(i + 1);
+        camelCase = camelCase.slice(0, i) + " " + camelCase.slice(i++);
       }
     }
     return camelCase;
   }
 
-  this.render = () => {
-    tbody.innerHTML = "";
+  function render() {
+    tbody.innerHTML = '';
     data.forEach(el => {
       let row = document.createElement("tr");
-      propNames.forEach(
-        prop => (row.innerHTML += '<td class="v-middle">' + el[prop] + "</td>")
-      );
+      propNames.forEach(prop => row.innerHTML += '<td class="v-middle">' + el[prop] + "</td>");
 
       let btnContainer = document.createElement("td");
       btnContainer.className = "d-flex justify-content-between";
@@ -60,8 +93,8 @@ function ObjectTableManager(tableName, data, container) {
       let btnUpdate = document.createElement("button");
       btnUpdate.className = "btn btn-warning btn-crud";
       btnUpdate.innerHTML = `
-        <span class="d-none d-md-inline">Update</span>
-        <span class="d-inline d-md-none">&#8796;</span>`;
+      <span class="d-none d-md-inline">Update</span>
+      <span class="d-inline d-md-none">&#8796;</span>`;
 
       let btnDelete = document.createElement("button");
       btnDelete.className = "btn btn-danger btn-crud";
@@ -82,7 +115,7 @@ function ObjectTableManager(tableName, data, container) {
       btnDelete.addEventListener("click", () => {
         if (!objectBeingEdited) {
           deleteObject(el.id);
-          this.render();
+          render();
         } else {
           alert("Pabaikite keisti elementą");
         }
@@ -93,18 +126,6 @@ function ObjectTableManager(tableName, data, container) {
       row.appendChild(btnContainer);
       tbody.appendChild(row);
     });
-  };
-
-  function deleteObject(id) {
-    data = data.filter(el => el.id != id);
-  }
-
-  function saveObject(object){
-
-  }
-
-  function updateObject(object){
-
   }
 
   function setEditable(btnUpdate, id, editableTags) {
@@ -118,25 +139,42 @@ function ObjectTableManager(tableName, data, container) {
     let btnSave = document.createElement("button");
     btnSave.className = "btn btn-success btn-crud";
     btnSave.innerHTML = `
-      <span class="d-none d-md-inline">Save</span>
-      <span class="d-inline d-md-none">&#x2713;</span>`;
+    <span class="d-none d-md-inline">Save</span>
+    <span class="d-inline d-md-none">&#x2713;</span>`;
     btnUpdate.parentNode.insertBefore(btnSave, btnUpdate);
     btnUpdate.remove();
     btnSave.addEventListener("click", () => {
       objectBeingEdited = false;
-      // Baigiau ties  gero bdo ieškojimu išsitrauk reikšmes iš HTML kolekcijos
-      let values = editableTags.map(el => el.value);
-      console.log(values);
-      saveObject(id, formObject({id}));
+      let values = editableTags.map(el => el.innerHTML);
+      let formedObject = formObject(id, values);
+      updateObject(formedObject);
       render();
     });
   }
-}
 
-function formObject(){
+  function deleteObject(id) {
+    data = data.filter(el => el.id != id);
+  }
 
+  function saveObject(object){
+    data.push(object);
+  }
+
+  function updateObject(object) {
+    data = data.map(ob => {
+      if (object.id == ob.id) return object;
+      return ob;
+    });
+  }
+
+  function formObject(id, array) {
+    let object = {
+      id
+    };
+    array.forEach((el, i) => object[propNames[i]] = el);
+    return object;
+  }
 }
 
 let carContainer = document.querySelector(".js-cars-container");
-let carManager = new ObjectTableManager("Car manager", cars, carContainer); // Duomenys ir konteineris
-carManager.render();
+let carManager = new ObjectManager("Car manager", cars, carContainer);
