e24971980b47b368671253283d1b1037be0e542b
Žilvinas <Zilvinas.vidmantas.93@gmail.com>
2019-03-22 20:39:40 +0200

    Table display from DB

--- MEDŽIAGA/php/16/mvc/models/CatalogModel.php
+++ MEDŽIAGA/php/16/mvc/models/CatalogModel.php
@@ -3,5 +3,12 @@
 class CatalogModel extends Model{
   public function __construct() {
     parent::__construct();
+
+  }
+  public function getBooks(){
+    $query = 'SELECT * FROM `book`';
+    $pdoStatement = $this->db->prepare($query);
+    $pdoStatement->execute();
+    var_dump($pdoStatement->fetchAll(PDO::FETCH_ASSOC));
   }
 }
